d1a282550374a9d7828d67f7df84173f3cfbc301
Omar <jabour.omar@hotmail.com>
2025-09-18 19:39:37 +0200

    working in Nav Coponent

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -10,7 +10,7 @@ const Nav = () => {
           <div className='w-10 h-10 bg-pink-500 rounded-full flex items-center justify-center flex-col'>
             <GrTechnology className='h-6 w-6 text-white'/>
           </div>
-          <h1 className='text-xl hidden'>Agency Management</h1>
+          <h1 className='text-xl hidden sm:block md:text-2xl'>Agency Management</h1>
         </div>
       </div>
     </div>
