254a7dcce1f09619aa4ccf85779973a7b3fa8051
Omar <jabour.omar@hotmail.com>
2025-09-18 19:37:35 +0200

    working in Nav Coponents

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>
-      <div>
+    <div className='transition-all duration-200 h-[12vh] z-[100] fixed w-full bg-blue-900'>
+      <div className='flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto'>
         {/* LOGO */}
-        <div>
+        <div className='flex items-center space-x-2'>
           <div></div>
           <h1 className='text-xl hidden'>Agency Management</h1>
         </div>
