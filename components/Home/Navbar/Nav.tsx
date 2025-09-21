f7db07b102fc0921d46fb30cd2c08385f95c4e34
Omar <jabour.omar@hotmail.com>
2025-09-21 02:17:57 +0200

    Working on the navs

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -33,7 +33,9 @@ const Nav = () => {
                from-purple-600 to-blue-500'></span>
               <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter
               group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500'></span>
-              <span className='absolute inset-0 w-full h-full transition-all'></span>
+              <span className='absolute inset-0 w-full h-full transition-all duration-200 ease-out
+              rounded shadow-xl bg-gradient-to-br filter group-active:opacity-0 group-hover:blur-sm
+              from-purple-600 to-blue-500'></span>
             </a>
         </div>
       </div>
