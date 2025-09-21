5c0a417dc1ac1ca8896043f3d791a7e2c7a1ea42
Omar <jabour.omar@hotmail.com>
2025-09-21 02:15:31 +0200

    Working on the navs

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -31,6 +31,8 @@ const Nav = () => {
             <a href="#" className='px-5 py-2.5 relative rounded group font-medium text-white inline-block'>
               <span className='absolute top-0 left-0 w-full h-full rounded opacity-50 filter blur-sm bg-gradient-to-br
                from-purple-600 to-blue-500'></span>
+              <span className='h-full w-full inset-0 absolute mt-0.5 ml-0.5 bg-gradient-to-br filter
+              group-active:opacity-0 rounded opacity-50 from-purple-600 to-blue-500'></span>
             </a>
         </div>
       </div>
